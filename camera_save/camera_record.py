cdac7285e7d8a4f27982e9782bb15b150a07396c
root <coneypo@gmail.com>
2019-08-02 17:47:58 +0800

    debug

--- camera_save/camera_record.py
+++ camera_save/camera_record.py
@@ -1,7 +1,13 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+fourcc = cv2.VideoWriter_fourcc('F', 'L', 'V', '1')
+# fourcc = cv2.VideoWriter_fourcc('m', 'p', 'e', 'g')
+# fourcc = cv2.VideoWriter_fourcc(*'mpeg')
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+# fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+
 fps = cap.get(cv2.CAP_PROP_FPS)
 
 print(fps)
@@ -10,15 +16,11 @@ print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 # out = cv2.VideoWriter('camera_test.avi', fourcc,10.0, size)
-out = cv2.VideoWriter('camera_test.avi', fourcc,10.0, size, True)
+out = cv2.VideoWriter('acrn_test.flv', fourcc, 30.0, size, True)
 while True:
     ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
-    # print(frame.size)
-    # print(len(frame))
-    # print(len(frame[0]))
+    # frame = cv2.flip(frame, 1)
     out.write(frame)
-    # 在图像上显示 Press Q to save and quit
     cv2.putText(frame,
                 "Press Q to save and quit",
                 (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
--- camera_save/camera_ss.py
+++ camera_save/camera_ss.py
@@ -1,36 +1,31 @@
 import cv2
-import time
 import os
 import sys
-
 
 path_screenshots = sys.path[0]+'/'
 cap = cv2.VideoCapture(0)
 cap.set(3, 960)
 
-ss_cnt = 0
+file_list=os.listdir(sys.path[0])
 
-while cap.isOpened():
-    flag, img_rd = cap.read()
-    k = cv2.waitKey(1)
-    img_gray = cv2.cvtColor(img_rd, cv2.COLOR_RGB2GRAY)
-    font = cv2.FONT_HERSHEY_SIMPLEX
+img_list=[]
+img_cnt_max=0
+for file in file_list:
+    if '.jpg' in file:
+        img_list.append(file)
+        img_cnt=str(file).split('.')[0].split('_')[-1]
+        img_cnt=int(img_cnt)
+        if img_cnt>img_cnt_max:
+            img_cnt_max=img_cnt
+        else:
+            img_cnt_max=img_cnt_max
 
-    if k == ord('q'):
-        break
-    else:
-        cv2.putText(img_rd, "Press 'S': Screen shot", (20, 400), font, 0.8, (255, 255, 255), 1, cv2.LINE_AA)
-        cv2.putText(img_rd, "Press 'Q': Quit", (20, 450), font, 0.8, (255, 255, 255), 1, cv2.LINE_AA)
-    if k == ord('s'):
-        ss_cnt += 1
-        print(path_screenshots + "screenshot" + "_" + str(ss_cnt) + "_" + time.strftime("%Y-%m-%d-%H-%M-%S",
-                                                                                        time.localtime()) + ".jpg")
-        cv2.imwrite(path_screenshots + "screenshot" + "_" + str(ss_cnt) + "_" + time.strftime("%Y-%m-%d-%H-%M-%S",
-                                                                                              time.localtime()) + ".jpg",
-                    img_rd)
+print('Existing images:', img_list)
 
-    cv2.namedWindow("camera", 1)
-    cv2.imshow("camera", img_rd)
+cnt='2'
 
-cap.release()
-cv2.destroyAllWindows()
+flag, img_rd = cap.read()
+k = cv2.waitKey(1)
+img_gray = cv2.cvtColor(img_rd, cv2.COLOR_RGB2GRAY)
+cv2.imwrite(path_screenshots +"acrn_test_"+str(img_cnt_max+1)+".jpg",img_rd)
+print('Saving into:    ', path_screenshots +"acrn_test_"+str(img_cnt_max+1)+".jpg")
